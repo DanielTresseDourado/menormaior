710afae5705fc883b0a3ddda1cdb2611b422ca40
Usuario <Usuario@DESKTOP-9EB77DJ>
2023-02-14 21:54:14 -0300

    conferir o programa

--- menormaior/main.c
+++ menormaior/main.c
@@ -6,5 +6,20 @@ main() {
 	int b;
 
 	printf("primeiro número:");
-		scanf("%d", a);
+	scanf("%d", &a);
+	printf("segundo número:");
+	scanf("%d", &b);
+
+	while (1) {
+
+		if (a > b) {
+			printf("primeiro numero maior que o segundo");
+		}
+		else if (a < b) {
+			printf("primeiro numero menor que o segundo");
+		}
+		else(a == b) {
+			printf("primeiro numero igual ao segundo");
+		}
+	}
 }
